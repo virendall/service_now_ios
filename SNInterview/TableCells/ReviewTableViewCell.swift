96e1395874bcf8cb225901957063900f75e19b1f
Virender <viru.dall@gmail.com>
2022-04-28 15:26:37 +0530

    ReviewTableViewCell to show the review's using CoffeeShopItemView

---
+++ SNInterview/TableCells/ReviewTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewTableViewCell.swift
+//  SNInterview
+//
+//  Created by Virender Dall on 28/04/22.
+//  Copyright © 2022 ServiceNow. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+    @IBOutlet weak var reviewView: CoffeeShopItemView!
+}
